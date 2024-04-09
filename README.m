9510ae5400882da8e848b5c5d6684b00057d2551
JM.developer <166416422+kaaaaoi999@users.noreply.github.com>
2024-04-09 15:58:54 +0600

    Update and rename README.m to README.htm

--- README.m
+++
@@ -1,8 +0,0 @@
-# Python-password-manager
-This includes the instant download for Your Products, Just click "add file to upload" to download it, and you can alternatively download from the 
-official website: https://sites.google.com/view/code-manager-org?usp=sharing |
-
-Technical Info:
-Number of passwords it can hold: 6
-deafault security/entry key: 8056
-Developed By Language : Python 3.11 (64-Bit)
