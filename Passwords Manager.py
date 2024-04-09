41a041e07adf4301063878112f2e3044f9bd9796
JM.developer <166416422+kaaaaoi999@users.noreply.github.com>
2024-04-09 15:41:52 +0600

    Add files via upload

---
+++ Passwords Manager.py
@@ -0,0 +1,14 @@
+var = p = input("entry key: ")
+if p == "8056": var = s = input("search passwords: ")
+else: exit("Your entry key was incorrect !")
+if s == "Google": print("Google Password: ")
+if s == "GitHub": print("GitHub Password: ")
+if s == "Embarcadero": print("Embarcadero Password: ")
+if s == "DuckDuckGo": print("DuckDuckGo Password: ")
+if s == "Instagram": print("Instagram Password: ")
+if s == "Opera": print("Opera Password: ")
+else: print("There are No More Passwords set !")
+# if there is a error message saying NameError: name 's' is not defined, your entry key may be incorrect,
+# and also replace: <replace this green/deep yellow text with your entry key (text only)> on the second line with your entry key.
+# entry key means: the user's verification code before entry.
+# best view on laptops, desktops, PC
